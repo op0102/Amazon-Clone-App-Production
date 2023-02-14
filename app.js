a3892a13be6b4aa4e08e8f3a3014d072acb3a1eb
op0102 <omprakash.chetiyar@gmail.com>
2023-02-14 20:59:00 +0530

    code-added

---
+++ app.js
@@ -0,0 +1,55 @@
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./db/connect");
+const app = express();
+const PORT = process.env.PORT || 8005;
+const cors = require("cors");
+const morgan = require("morgan");
+const router = require("./routes/router");
+const cookieParser = require("cookie-parser");
+
+
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     next();
+//   });
+
+
+
+
+
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser(""));
+app.use(morgan("dev"));
+
+
+app.use(router);
+
+//static files
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+
+
+
+
+app.get("/", (req, res) => {
+    res.send("Hi! I am Omprakash Live in Amazon Clone")
+});
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URL);
+
+        app.listen(PORT, () => {
+            console.log(`${PORT} Yes i am connected`);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+start();
